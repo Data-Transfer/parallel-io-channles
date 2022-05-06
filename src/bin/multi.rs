91f3c66fa0fbdf604bea5683fac6db3658f57870
Ugo Varetto <ugovaretto@gmail.com>
2022-05-06 22:28:36 +0800

    ...

--- src/bin/multi.rs
+++ src/bin/multi.rs
@@ -1,13 +1,14 @@
 use std::fs::File;
 use std::io::Read;
+use std::io::SeekFrom;
 use std::sync::mpsc;
 use std::sync::mpsc::Sender;
 use std::thread;
 
 type Buffer = Vec<u8>;
-
+type Offset = u64;
 enum FileBuf {
-    MemBuffer(Buffer, Sender<FileBuf>), //, Arc<Receiver<FileBuf>>),
+    MemBuffer(Buffer, Offset, Sender<FileBuf>), //, Arc<Receiver<FileBuf>>),
 }
 
 /// Separate file read from data consumption using a fixed amount of memory.
@@ -18,20 +19,23 @@ enum FileBuf {
 /// to return the buffer to the sender.
 fn main() {
     let filename = std::env::args().nth(1).expect("Missing file name");
-    let (tx1, rx1) = mpsc::channel::<FileBuf>();
-    let (tx2, rx2) = mpsc::channel::<FileBuf>();
+    let producers = (0..4).map(|_| channel::<FileBuf>()).collect::<Vec::<_>>();
+    let consumers = (0..2).map(|_| channel::<FileBuf>()).collect::<Vec::<_>>();
     let mut file = File::open(filename).expect("Cannot open file");
     let chunk_size = 128;
     for _ in 0..2 {
         tx1.send(FileBuf::MemBuffer(vec![0_u8; 128], tx1.clone()))
             .expect("Error sending buffer");
     }
-    let _read_file = thread::spawn(move || {
-        while let Ok(FileBuf::MemBuffer(mut buffer, tx1)) = rx1.recv() {
+    for i in 0..4 {
+    thread::spawn(move || {
+        while let Ok(FileBuf::MemBuffer(mut buffer, offset, tx1)) = rx1.recv() {
+            buffer.reserve(chunk_size);
             assert!(buffer.capacity() >= chunk_size);
             unsafe { 
                 buffer.set_len(chunk_size);
             }
+            file.seek(SeekFrom::Start(offset));
             match file.read(&mut buffer) {
                 Ok(0) | Err(_) => {
                     tx2.send(FileBuf::MemBuffer(Vec::new(), tx1))
@@ -49,17 +53,23 @@ fn main() {
             }
         }
     });
-    let print_values = thread::spawn(move || {
-        while let Ok(FileBuf::MemBuffer(buffer, tx)) = rx2.recv() {
-            if buffer.len() == 0 {
-                return;
-            }
-            let t = String::from_utf8_lossy(&buffer);
-            println!("{}", t);
-            if let Err(_) = tx1.send(FileBuf::MemBuffer(buffer, tx.clone())) {
-                return;
-            }
-        }
-    });
+}
+    let consumers_handles = Vec::new();
+    for i in 0..2 {
+        let rx = consumers[i].1;;
+        let h = thread::spawn(move || {
+                    while let Ok(FileBuf::MemBuffer(buffer, tx)) = rx.recv() {
+                        if buffer.len() == 0 {
+                            return;
+                        }
+                        let t = String::from_utf8_lossy(&buffer);
+                        println!("{}", t);
+                        if let Err(_) = tx.send(FileBuf::MemBuffer(buffer, tx.clone())) {
+                            return;
+                        }
+                    }
+        });
+        consumers_handles.push(h);
+    }
     print_values.join().expect("Error joining thread");
 }
